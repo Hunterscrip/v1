1c2cf455af5f15fd01b4feb064c09ead13849347
Hunterscrip <138548175+Hunterscrip@users.noreply.github.com>
2023-07-10 17:04:48 +0800

    Update nontls.sh

--- websocket/nontls.sh
+++ websocket/nontls.sh
@@ -16,19 +16,19 @@ red='\e[1;31m'
 green='\e[0;32m'
 yell='\e[1;33m'
 NC='\e[0m'
-echo "Installing SSH Websocket None TLS by RstoreVPN " | lolcat
+echo "Installing SSH Websocket None TLS by RMBL VPN " | lolcat
 echo "Progress..." | lolcat
 sleep 3
 
 # Getting Proxy Template
-wget -q -O /usr/local/bin/ws-nontls https://raw.githubusercontent.com/myridwan/VVIP/ipuk/websocket/ws-nontls.py
+wget -q -O /usr/local/bin/ws-nontls https://raw.githubusercontent.com/Hunterscrip/v1/websocket/ws-nontls.py
 chmod +x /usr/local/bin/ws-nontls
 
 # Installing Service
 cat > /etc/systemd/system/ws-nontls.service << END
 [Unit]
-Description=Python Proxy Mod By Arh-Project
-Documentation=https://t.me/r1f4n_1122
+Description=Python Proxy Mod RMBLVPN 
+Documentation=https://t.me/rmblvpn
 After=network.target nss-lookup.target
 
 [Service]
