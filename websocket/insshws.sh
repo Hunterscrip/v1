a17efd3f3fcd4ccf0274059f92f291515888df33
Hunterscrip <138548175+Hunterscrip@users.noreply.github.com>
2023-07-10 17:02:43 +0800

    Update insshws.sh

--- websocket/insshws.sh
+++ websocket/insshws.sh
@@ -16,20 +16,20 @@ red='\e[1;31m'
 green='\e[0;32m'
 yell='\e[1;33m'
 NC='\e[0m'
-echo "Installing SSH Websocket by RstoreVPN" | lolcat
+echo "Installing SSH Websocket by RMBL VPN" | lolcat
 echo "Progress..." | lolcat
 sleep 3
 cd
 
-wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/myridwan/VVIP/ipuk/websocket/dropbear-ws.py
-wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/myridwan/VVIP/ipuk/websocket/ws-stunnel
+wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/Hunterscrip/v1/websocket/dropbear-ws.py
+wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/Hunterscrip/v1/websocket/ws-stunnel
 
 chmod +x /usr/local/bin/ws-dropbear
 chmod +x /usr/local/bin/ws-stunnel
 
-wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/myridwan/VVIP/ipuk/websocket/service-wsdropbear && chmod +x /etc/systemd/system/ws-dropbear.service
+wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/Hunterscrip/v1/websocket/service-wsdropbear && chmod +x /etc/systemd/system/ws-dropbear.service
 
-wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/myridwan/VVIP/ipuk/websocket/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
+wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/Hunterscrip/v1/websocket/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
 
 systemctl daemon-reload
 systemctl enable ws-dropbear.service
